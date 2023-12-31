96b09877651f3d95971f2cd87aaaedc7f54d42f0
Himanshi <141846224+HimanshiEnigma@users.noreply.github.com>
2023-12-31 06:11:32 -0800

    Create Diameter of a tree.java

---
+++ Diameter of a tree.java
@@ -0,0 +1,52 @@
+public class binaryTreeB {
+    static class Node {
+        int data;
+        Node left, right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int lh = height(root.left);
+        int rh = height(root.right);
+        return Math.max(lh, rh) + 1;
+    }
+
+    public static int Diam(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDiam = Diam(root.left);
+        int leftHt= height(root.left);
+        int rightDiam= Diam(root.right);
+        int rightHt=height(root.right);
+
+        int SelfDiam = leftHt+rightHt+1;
+        return Math.max(SelfDiam,Math.max(leftDiam,rightDiam));
+
+    }
+    public static void main(String[] args) {
+        /*  1
+          /   \
+         2     3
+        / \   / \
+        4  5 6   7
+
+         */
+
+     Node root= new Node(1);
+     root.left= new Node(2);
+     root.right= new Node(3);
+     root.left.left= new Node(4);
+     root.right.right= new Node(5);
+     root.right.left= new Node(6);
+        System.out.print(Diam(root));
+    }
+}
